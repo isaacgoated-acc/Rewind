9cec0ffc2e5a4deb7a60ba243d3d6683bc4addfb
isaacgoated-acc <canijustrecord@gmail.com>
2026-06-13 15:25:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static("public"));
+// serve website files
+app.use(express.static(path.join(__dirname, "public")));
 
+// homepage route
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Rewind server running on port " + PORT);
+  console.log("Rewind running on port " + PORT);
 });
